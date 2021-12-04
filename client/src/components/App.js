74e76546493e735ffc65e4c9165a8445e982472f
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-04 13:44:38 +0100

    Create project structure, client and server and Auth routes

---
+++ client/src/components/App.js
@@ -0,0 +1,63 @@
+import { Component } from 'react'
+import './App.css'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Navbar from './layout/Navigation/Navbar'
+import SignupPage from './pages/Signup/SignupPage'
+import LoginPage from './pages/Login/LoginPage'
+import AuthService from '../services/auth.service'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loggedUser: undefined,
+    }
+
+    this.authService = new AuthService()
+  }
+
+  componentDidMount() {
+    this.authService
+      .isloggedin()
+      .then((response) => this.storeUser(response.data))
+      .catch((err) => this.storeUser(null))
+  }
+
+  storeUser = (user) => {
+    this.setState({ loggedUser: user })
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar storeUser={this.storeUser} loggedUser={this.state.loggedUser} />
+
+        <main>
+          <Switch>
+            {this.state.loggedUser ? (
+              <Redirect to="coaster-list" />
+            ) : (
+              <>
+                <Route
+                  path="/signup"
+                  render={(props) => (
+                    <SignupPage {...props} storeUser={this.storeUser} />
+                  )}
+                />
+                <Route
+                  path="/login"
+                  render={(props) => (
+                    <LoginPage {...props} storeUser={this.storeUser} />
+                  )}
+                />
+              </>
+            )}
+          </Switch>
+        </main>
+      </>
+    )
+  }
+}
+
+export default App
