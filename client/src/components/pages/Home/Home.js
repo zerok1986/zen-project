e0d84ea547704769ec1914e7b3428608ead1f378
Miguel Munoz Estelles <mmestelles@gmail.com>
2021-12-07 19:19:30 +0100

    Home html done, need to rethink conditions in app (modals or not)

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,10 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import './Home.css'
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Container>
+            <Row className="justify-content-around description">
+                <Col md={12} style={{ overflow: "hidden" }}>
+                <p>
+                    Bienvenido a nuestra comunidad Zen. Encuentra las clases más cercanas a tu localización, tanto en centros especializados como al aire libre. Agenda y reserva tus clases con un solo click.
+                </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Link to="/signup">
+                        <Button variant="outline-success">Regístrate</Button>
+                    </Link>
+                </Col>
+                <Col>
+                    <Link to="/login">
+                        <Button variant="outline-secondary">Inicia Sesión</Button>
+                    </Link>
+                </Col>
+            </Row>
+            <Row className="justify-content-around">
+                <Col md={4}>
+                    <img src="#" alt="Yoga"/>
+                    <h1>Yoga</h1>
+                    <p>
+                        Párrafo bonito sobre yoga
+                    </p>
+                </Col>
+                <Col md={4}>
+                    <img src="#" alt="Taichí"/>
+                    <h1>Taichí</h1>
+                    <p>
+                        Párrafo bonito sobre Taichí.
+                    </p>
+                </Col>
+                <Col md={4}>
+                    <img src="#" alt="Meditación"/>
+                    <h1>Meditación</h1>
+                    <p>
+                        Párrafo bonito sobre Meditación.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
