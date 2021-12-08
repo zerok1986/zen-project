58407306cebb9b846cd43339f03de77db0bac4ec
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-08 23:39:26 +0100

    (CR-SERVER/CLIENT): General cleaning, comments, etc

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,4 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require('mongoose')
-
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 const DB_REMOTE = process.env.DB_REMOTE
 
 mongoose
