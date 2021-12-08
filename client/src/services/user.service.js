44bf31209047952902948982468a6123bec097db
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-08 23:24:22 +0100

    (CR-SERVER): Modify routes to fetch /api and delete node_modules folder from project's root tree

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class UserService {
   constructor() {
     this.app = axios.create({
-      baseURL: 'http://localhost:5000/users',
+      baseURL: 'http://localhost:5000/api/users',
     })
   }
 
