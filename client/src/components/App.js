9567b70d0197e4008809562ab1219433bfbd8377
marferbl <fernandezblascomarcos@gmail.com>
2021-12-08 14:00:57 +0100

    again conflickts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,11 +5,6 @@ import Navbar from './layout/Navigation/Navbar'
 import AuthService from '../services/auth.service'
 import Portal from './pages/Portal/Portal'
 import Home from './pages/Home/Home'
-<<<<<<< HEAD
-import ActivityDetails from './pages/Portal/ActivitiesList/Activity/ActivityDetails/ActivityDetails'
-=======
-
->>>>>>> 75b0f2a55831c9e2ab7f48180411344607646658
 const authService = new AuthService()
 
 const App = (props) => {
@@ -31,35 +26,7 @@ const App = (props) => {
       <UserProvider value={{ loggedUser, storeUser }}>
         <Navbar {...props} />
         <main>
-<<<<<<< HEAD
-          <Switch>
-            <Route
-              path="/activities/activity/:id"
-              render={(props) => <ActivityDetails {...props} />}
-            />
-
-            {loggedUser ? (
-              <Redirect to="/portal" />
-            ) : (
-              <>
-                <Route
-                  path="/signup"
-                  render={(props) => (
-                    <SignupPage {...props} storeUser={storeUser} />
-                  )}
-                />
-                <Route
-                  path="/login"
-                  render={(props) => (
-                    <LoginPage {...props} storeUser={storeUser} />
-                  )}
-                />
-              </>
-            )}
-          </Switch>
-=======
           {loggedUser ? <Portal /> : <Home storeUser = {storeUser}/>}
->>>>>>> 75b0f2a55831c9e2ab7f48180411344607646658
         </main>
       </UserProvider>
     </>
