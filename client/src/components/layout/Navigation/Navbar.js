e454d336823a5f869b3d9bae49698af1f80fb5bf
marferbl <fernandezblascomarcos@gmail.com>
2021-12-13 16:47:57 +0100

    home styles

--- client/src/components/layout/Navigation/Navbar.js
+++ client/src/components/layout/Navigation/Navbar.js
@@ -1,54 +1,50 @@
-import React, { useState, useContext } from 'react'
-import { Navbar, Nav, Container, Modal } from 'react-bootstrap'
-import UserContext from '../../../context/UserContext'
-import { Link } from 'react-router-dom'
-import AuthService from '../../../services/auth.service'
-import SignupPage from '../../pages/Signup/SignupPage'
-import LoginPage from '../../pages/Login/LoginPage'
-import './Navbar.css'
+import React, { useState, useContext } from "react";
+import { Navbar, Nav, Container, Modal } from "react-bootstrap";
+import UserContext from "../../../context/UserContext";
+import { Link } from "react-router-dom";
+import AuthService from "../../../services/auth.service";
+import SignupPage from "../../pages/Signup/SignupPage";
+import LoginPage from "../../pages/Login/LoginPage";
+import "./Navbar.css";
 
-const authService = new AuthService()
+const authService = new AuthService();
 
 const Navigation = (props) => {
-  const { loggedUser, storeUser, setDetailsClick } = useContext(UserContext)
+  const { loggedUser, storeUser, setDetailsClick } = useContext(UserContext);
 
-  const [showModal, setModal] = useState(false)
-  const [modalType, setType] = useState('')
+  const [showModal, setModal] = useState(false);
+  const [modalType, setType] = useState("");
 
   const openModal = () => {
-    setModal(true)
-  }
+    setModal(true);
+  };
 
   const closeModal = () => {
-    setModal(false)
-    setType('')
-  }
+    setModal(false);
+    setType("");
+  };
 
   const logout = () => {
     authService
       .logout()
       .then((response) => storeUser(null))
-      .catch((err) => console.error(err))
-  }
+      .catch((err) => console.error(err));
+  };
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar className="navbar-main" collapseOnSelect expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/home">NOMBRE ZEN</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="/home">moksha</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggle-icon" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/home">Inicio</Nav.Link>
             {loggedUser ? (
               <>
-                <Nav.Link
-                  as={Link}
-                  onClick={setDetailsClick}
-                  to={`/users/user/${loggedUser._id}`}
-                >
+                <Nav.Link as={Link} onClick={setDetailsClick} to={`/users/user/${loggedUser._id}`}>
                   Mi perfil
                 </Nav.Link>
-                <Nav.Link as={'span'} onClick={logout}>
+                <Nav.Link as={"span"} onClick={logout}>
                   Cerrar sesión
                 </Nav.Link>
               </>
@@ -56,16 +52,16 @@ const Navigation = (props) => {
               <>
                 <Nav.Link
                   onClick={() => {
-                    setType('Sign up')
-                    openModal()
+                    setType("Sign up");
+                    openModal();
                   }}
                 >
                   Registro
                 </Nav.Link>
                 <Nav.Link
                   onClick={() => {
-                    setType('Log in')
-                    openModal()
+                    setType("Log in");
+                    openModal();
                   }}
                 >
                   Iniciar Sesión
@@ -76,11 +72,7 @@ const Navigation = (props) => {
                     <Modal.Title>{modalType}</Modal.Title>
                   </Modal.Header>
                   <Modal.Body>
-                    {modalType === 'Sign up' ? (
-                      <SignupPage closeModal={closeModal} />
-                    ) : (
-                      <LoginPage closeModal={closeModal} />
-                    )}
+                    {modalType === "Sign up" ? <SignupPage closeModal={closeModal} /> : <LoginPage closeModal={closeModal} />}
                   </Modal.Body>
                 </Modal>
                 {/* MODAL */}
@@ -90,7 +82,7 @@ const Navigation = (props) => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
