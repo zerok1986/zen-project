e85747a8bbeb5e2f7450d808274dc14d62a9e0a4
marferbl <fernandezblascomarcos@gmail.com>
2021-12-14 17:39:11 +0100

    Button edit, route protected edit user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-const { isLoggedIn } = require('../middlewares')
+const { isLoggedIn } = require("../middlewares");
 
 module.exports = (app) => {
-  app.use('/api', require('./auth.routes'))
-  app.use('/api/activities', isLoggedIn, require('./activities.routes'))
-  app.use('/api/users', isLoggedIn, require('./user.routes'))
-  app.use('/api/reviews', isLoggedIn, require('./review.routes'))
-  app.use('/api/upload', isLoggedIn, require('./upload.routes'))
-}
+  app.use("/api", require("./auth.routes"));
+  app.use("/api/activities", isLoggedIn, require("./activities.routes"));
+  app.use("/api/users", isLoggedIn, require("./user.routes"));
+  app.use("/api/reviews", isLoggedIn, require("./review.routes"));
+  app.use("/api/upload", require("./upload.routes"));
+};
