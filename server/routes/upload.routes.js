3fdcbbcbba756a9669e14ec8d93bda88cce354b7
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-14 10:14:32 +0100

    Include middlewares to protect routes in the backend

--- server/routes/upload.routes.js
+++ server/routes/upload.routes.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const uploader = require("../config/cloudinary.config");
+const router = require('express').Router()
+const uploader = require('../config/cloudinary.config')
 
-router.post("/image", uploader.single("imageData"), (req, res) => {
+router.post('/image', uploader.single('imageData'), (req, res) => {
   if (!req.file) {
-    res.status(500).json({ code: 500, message: "Error loading the file" });
-    return;
+    res.status(500).json({ code: 500, message: 'Error loading the file' })
+    return
   }
 
-  res.json({ cloudinary_url: req.file.path });
-});
+  res.json({ cloudinary_url: req.file.path })
+})
 
-module.exports = router;
+module.exports = router
