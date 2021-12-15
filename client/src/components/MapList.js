69d354005950a9b35e4a552b3e9732cd2b7e2822
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-15 15:56:08 +0100

    Add infoWindow component to each Marker with link to ActivityDetails

--- client/src/components/MapList.js
+++ client/src/components/MapList.js
@@ -1,20 +1,32 @@
-import React from "react";
-import { GoogleMap, LoadScript, Marker, Circle } from "@react-google-maps/api";
-import mapStyles from "./mapStyles";
+import React, { useState, useContext } from 'react'
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Circle,
+  InfoWindow,
+} from '@react-google-maps/api'
+import { Link } from 'react-router-dom'
+import mapStyles from './mapStyles'
+import UserContext from '../context/UserContext'
 
 const containerStyle = {
-  width: "400px",
-  height: "400px",
-  border: "1px solid black"
-};
-const zoom = 14;
-const API_KEY = "AIzaSyA2EY6nvc3be8-6agfTwW2PNHPH0GX3dg8";
+  width: '400px',
+  height: '400px',
+  border: '1px solid black',
+}
+const zoom = 14
+const API_KEY = 'AIzaSyA2EY6nvc3be8-6agfTwW2PNHPH0GX3dg8'
 
 const MapList = (props) => {
+  const [selectedActivity, setSelectedActivity] = useState(null)
+
+  const { setDetailsClick } = useContext(UserContext)
+
   const center = {
     lat: props.location.coordinates[0],
     lng: props.location.coordinates[1],
-  };
+  }
 
   return (
     <LoadScript googleMapsApiKey={API_KEY}>
@@ -31,27 +43,53 @@ const MapList = (props) => {
             center={center}
             radius={1250}
             options={{
-              fillColor: "rgba(110, 134, 214, 0.404)",
+              fillColor: 'rgba(110, 134, 214, 0.404)',
               strokeOpacity: 0.3,
             }}
           />
-          )
-        }
+        )}
         {props.activities.map((activity) => {
           return (
             <Marker
-              icon={"https://img.icons8.com/ios-glyphs/30/000000/marker--v1.png"}
+              icon={
+                'https://img.icons8.com/ios-glyphs/30/000000/marker--v1.png'
+              }
               key={activity._id}
               title={activity.name}
               position={{
                 lat: activity.location.coordinates[0],
                 lng: activity.location.coordinates[1],
               }}
+              onClick={() => {
+                setSelectedActivity(activity)
+              }}
             />
-          );
+          )
         })}
+
+        {selectedActivity && (
+          <InfoWindow
+            position={{
+              lat: selectedActivity.location.coordinates[0],
+              lng: selectedActivity.location.coordinates[1],
+            }}
+            onCloseClick={() => {
+              setSelectedActivity(null)
+            }}
+          >
+            <span>
+              <Link
+                className="sandwich"
+                onClick={() => setDetailsClick()}
+                to={`/activities/activity/${selectedActivity._id}`}
+              >
+                {selectedActivity.name}
+              </Link>
+            </span>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </LoadScript>
-  );
-};
-export default MapList;
+  )
+}
+export default MapList
